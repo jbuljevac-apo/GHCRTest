c4ccd22fae8ffb7bbd023ac63e3971b540fbe023
jbuljevac-apo <j.buljevac@group-apo.com>
2025-05-21 15:14:07 +0200

    test 8

--- .github/containerOverride.ps1
+++ .github/containerOverride.ps1
@@ -1,3 +1,5 @@
 # .github/containerOverride.ps1
 $containerImage = "ghcr.io/jbuljevac-apo/bc365nl:ltsc2019"
 Write-Host "##vso[task.setvariable variable=useContainerImage]$containerImage"
+Write-Host "##vso[task.setvariable variable=artifactUrl]''"
+Write-Host "##vso[task.setvariable variable=artifactVersion]''"
